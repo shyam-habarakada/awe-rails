c0baa74649752c63cc89acc7ea339300055299de
Shyam Habarakada <shyam.habarakada@gmail.com>
2012-02-12 14:13:54 -0800

    Fix bug in install action

--- lib/generators/awe/install/install_generator.rb
+++ lib/generators/awe/install/install_generator.rb
@@ -6,9 +6,9 @@ module Awe
 
       desc "This generator installs AWE (Artefact Web Extensions) into rails"
       source_root File.expand_path('../../../../../vendor/assets/javascripts', __FILE__)
-      javascripts_path = ::Rails.application.config.assets.enabled ? "vendor/assets/javascripts" : "public/javascripts"
 
       def copy_awe
+        javascripts_path = ::Rails.application.config.assets.enabled ? "vendor/assets/javascripts" : "public/javascripts"
         say_status("copying", "The AWE files", :green)
         copy_file "awe-core.js", "#{javascripts_path}/awe-core.js"
         copy_file "awe-state-machine.js", "#{javascripts_path}/awe-state-machine.js"
